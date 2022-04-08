f011699f97929b84410425e732493fd910372661
Elias Birkedal <eliasb@uia.no>
2022-04-08 08:57:27 +0200

    Switched tabs for spaces

--- ztier.sh
+++ ztier.sh
@@ -9,8 +9,8 @@ ENDCOL="\e[0m"
 
 # Check that script is running as root
 if [[ $EUID != 0 ]]; then
-	echo "Run script as root"
-	exit 1
+    echo "Run script as root"
+    exit 1
 fi
 
 # Load .env variables
@@ -23,38 +23,38 @@ NETWORK_NAME=$(zerotier-cli listnetworks | sed -n '2 p' | awk '{print $4}')
 
 if [ -z $NETWORK_ID ]
 then
-	read -p "Join a network? (Y/n): " CHOICE
+    read -p "Join a network? (Y/n): " CHOICE
 
-	if [ "$CHOICE" == "Y" ] || [ "$CHOICE" == "y" ] || [ -z $CHOICE ]
-	then
-		echo "Available networks:"
-        	echo
-        	echo "1. Elias_Network"
+    if [ "$CHOICE" == "Y" ] || [ "$CHOICE" == "y" ] || [ -z $CHOICE ]
+    then
+        echo "Available networks:"
+        echo
+        echo "1. Elias_Network"
 
-        	read -p "Connect to: " CHOICE
+        read -p "Connect to: " CHOICE
 
-        	if [ $CHOICE -eq 1 ]
-        	then
-                	echo
-			sudo zerotier-cli join $ELIAS_NETWORK; echo
-		fi
+        if [ $CHOICE -eq 1 ]
+        then
+            echo
+	        sudo zerotier-cli join $ELIAS_NETWORK; echo
+    fi
 
-	else
-		echo "Goodbye!"
-		exit 0
-	fi	
+    else
+        echo "Goodbye!"
+        exit 0
+    fi	
 
 else
-	echo "Connected to  ${NETWORK_NAME} ${NETWORK_ID}"
-	echo
-	read -p "Leave network? (Y/n): " CHOICE
-	if [ "$CHOICE" == "Y" ] || [ "$CHOICE" == "y" ] || [ -z $CHOICE ]
-	then
-		echo
-		sudo zerotier-cli leave ${NETWORK_ID}; echo
-	else
-		echo "Goodbye!"
-		exit 0
-	fi
+    echo "Connected to  ${NETWORK_NAME} ${NETWORK_ID}"
+    echo
+    read -p "Leave network? (Y/n): " CHOICE
+    if [ "$CHOICE" == "Y" ] || [ "$CHOICE" == "y" ] || [ -z $CHOICE ]
+    then
+        echo
+        sudo zerotier-cli leave ${NETWORK_ID}; echo
+    else
+        echo "Goodbye!"
+        exit 0
+    fi
 
 fi
